1735b6599f52143aa3a146cf595609e6c10b7c64
louis hoang <bawsing@gmail.com>
2014-09-16 15:58:10 -0400

    finished

--- guess_number.js
+++ guess_number.js
@@ -2,25 +2,20 @@ $(document).ready(function() {
   var maxNum = +$("#max_num").text();
   var hiddenNum = Math.floor(Math.random() * maxNum);
   var count = 0;
-
+  var userNum;
   var showGuessCount = function(){
     $(".guesses").removeClass("hidden");
     $(".guesses").append(userNum + ', ');
   };
-
   $("#hidden_num").html(hiddenNum);
-
   $("#guess").on("submit", function(event){
     event.preventDefault();
-    var userNum = +$("#user-input").val();
-
+    userNum = +$("#user-input").val();
     if(isNaN(userNum) || userNum > 100 || userNum < 0){
       alert("Please choose a number between 0 and " + maxNum);
     }else {
-
       count += 1;
       showGuessCount();
-
       var answer_box = $(this).parent().find("#answer_box");
       if (userNum < hiddenNum){
         answer_box.html(userNum + " is too low, try again...");
@@ -31,10 +26,8 @@ $(document).ready(function() {
       }
     }
   });
-
     $("#guess").on("reset", function(event){
       document.location.reload();
       alert("Starting new game!");
     });
-
 });
