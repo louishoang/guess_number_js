2c93d3463f3aae81496522e1672377338746d279
louis hoang <bawsing@gmail.com>
2014-09-16 15:40:13 -0400

    added validation

--- guess_number.js
+++ guess_number.js
@@ -1,30 +1,40 @@
 $(document).ready(function() {
-
   var maxNum = +$("#max_num").text();
-  var userNum = +$("#user-input").val();
   var hiddenNum = Math.floor(Math.random() * maxNum);
   var count = 0;
+
+  var showGuessCount = function(){
+    $(".guesses").removeClass("hidden");
+    $(".guesses").append(userNum + ', ');
+  };
+
   $("#hidden_num").html(hiddenNum);
 
   $("#guess").on("submit", function(event){
     event.preventDefault();
     userNum = +$("#user-input").val();
-    count += 1;
-    $(".guesses").removeClass("hidden");
-    $(".guesses").append(userNum + ', ');
-    if (userNum < hiddenNum){
-      $(this).parent().find("#answer_box").html(userNum + " is too low, try again...");
-    }else if (userNum > hiddenNum){
-      $(this).parent().find("#answer_box").html(userNum + " is too high, try again");
-    }else if (userNum === hiddenNum){
-      $(this).parent().find("#answer_box").html("You won in " + count + " guesses.");
+
+    if(isNaN(userNum)){
+      alert("Please choose a number between 0 and " + maxNum);
+    }else {
+
+      count += 1;
+      showGuessCount();
+
+      var answer_box = $(this).parent().find("#answer_box");
+      if (userNum < hiddenNum){
+        answer_box.html(userNum + " is too low, try again...");
+      }else if (userNum > hiddenNum){
+        answer_box.html(userNum + " is too high, try again");
+      }else if (userNum === hiddenNum){
+        answer_box.html("You won in " + count + " guesses.");
+      }
     }
   });
 
-  $("#guess").on("reset", function(event){
-    window.location.reload();
-    alert("New game started!")
-  });
+    $("#guess").on("reset", function(event){
+      document.location.reload();
+      alert("Starting new game!");
+    });
 
 });
-
